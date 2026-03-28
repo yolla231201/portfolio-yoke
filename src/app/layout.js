7230b0f1224bf57853743f70aeccc21dc931772d
YohanesKefi <72695877+yolla231201@users.noreply.github.com>
2026-03-28 23:26:48 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,17 +3,14 @@ import "./globals.css";
 export const metadata = {
   title: "Yohanes Kefi",
   description: "Web & Graphic Designer Portfolio",
+  verification: {
+    google: "Yf33PHLDu9eq2PnTSev1IyRynZ2rHlpCi9S6H-gyrmo",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta
-          name="google-site-verification"
-          content="Yf33PHLDu9eq2PnTSev1IyRynZ2rHlpCi9S6H-gyrmo"
-        />
-      </head>
       <body className="font-[Montserrat]">{children}</body>
     </html>
   );
