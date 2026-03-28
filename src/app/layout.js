47bfb4979c6f94a082387578802a78524140d388
YohanesKefi <72695877+yolla231201@users.noreply.github.com>
2026-03-28 23:16:04 +0800

    Add Google site verification meta tag
    Added Google site verification meta tag to the layout.

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="Yf33PHLDu9eq2PnTSev1IyRynZ2rHlpCi9S6H-gyrmo"
+        />
+      </head>
       <body className="font-[Montserrat]">{children}</body>
     </html>
   );
